ca00c1cac8a24974b9f0786798385aabcedefdb5
jeong <ljwljw522@gmail.com>
2025-10-16 16:02:24 +0900

    feat: 애플리케이션 실행 및 기본 입출력 구조 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,29 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 문자열 입력받기
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+
+        try {
+            float result = StringCalculator.add(input);
+            printResult(result);
+        } catch (IllegalArgumentException | ArithmeticException e) {
+            System.out.println("[ERROR] " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("[ERROR] 예상치 못한 오류가 발생했습니다.");
+        }
+    }
+
+    // 결과 출력
+    private static void printResult(float result) {
+        if (result % 1 == 0) { // 정수 출력
+            System.out.println("결과 : " + (long) result);
+        } else { // 실수 출력
+            System.out.println("결과 : " + result);
+        }
     }
 }
---
+++ src/main/java/calculator/StringCalculator.java
@@ -0,0 +1,17 @@
+package calculator;
+
+public class StringCalculator {
+
+    /**
+     * 문자열 덧셈 계산기의 진입점
+     *  - 이후 단계에서 문자열 파싱, 검증, 계산 로직을 구현할 예정
+     */
+    public static float add(String input) {
+        // null 혹은 빈 문자열 입력시 0 반환
+        if (input == null || input.isBlank()){
+            return 0;
+        }
+
+        return 0;
+    }
+}
